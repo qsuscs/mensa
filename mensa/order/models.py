c4e1513cf8f78d085614143f269ba0905f68685f
Thomas Schneider <thosch97@gmail.com>
2014-02-06 21:16:13 +0100

    Added __unicode__ functions to Day and Meal class

--- mensa/order/models.py
+++ mensa/order/models.py
@@ -3,7 +3,13 @@ from django.db import models
 class Day(models.Model):
     date = models.DateTimeField('date')
 
+    def __unicode__(self):
+        return self.date.strftime('%Y-%m-%d')
+
 class Meal(models.Model):
     day = models.ForeignKey(Day)
     meal_text = models.CharField(max_length=200)
     orders = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.meal_text
