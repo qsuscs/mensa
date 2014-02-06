45dc09d9c6897012c25a96e8c20891c0932fb652
Thomas Schneider <thosch97@gmail.com>
2014-02-06 21:36:33 +0100

    Day: added is_from_this_week()

--- mensa/order/models.py
+++ mensa/order/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 class Day(models.Model):
     date = models.DateTimeField('date')
 
     def __unicode__(self):
         return self.date.strftime('%Y-%m-%d')
+
+    def is_from_this_week(self):
+        return self.date.strftime('%W') == timezone.now().strftime('%W') # TODO: Can be done better.
 
 class Meal(models.Model):
     day = models.ForeignKey(Day)
