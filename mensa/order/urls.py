d42aff1da90b0d49529512c0f5cf515ebca16e09
Thomas Schneider <thosch97@gmail.com>
2014-02-09 23:20:00 +0100

    Fixed the broken URLs

--- mensa/order/urls.py
+++ mensa/order/urls.py
@@ -4,11 +4,11 @@ from order import views
 
 urlpatterns = patterns('',
     # ex: /order/
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
     # ex: /order/42/
     url(r'^(?P<day_id>\d+)/$', views.detail, name='detail'),
     # ex: order/42/orders/
     url(r'^(?P<day_id>\d+)/orders/$', views.orders, name='orders'),
     # ex: /order/42/order/
-    url(r'^(?P<order_id>\d+/order/$', views.order, name='order'),
+    url(r'^(?P<day_id>\d+)/order/$', views.order, name='order'),
 )
