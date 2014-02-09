81d163a8059a9d1cdfea3123dbc1718b344cd390
Thomas Schneider <thosch97@gmail.com>
2014-02-09 23:02:57 +0100

    Added views, doesn't work though.

--- mensa/order/urls.py
+++ mensa/order/urls.py
@@ -3,5 +3,12 @@ from django.conf.urls import patterns, url
 from order import views
 
 urlpatterns = patterns('',
+    # ex: /order/
     url(r'^$', views.index, name='index')
+    # ex: /order/42/
+    url(r'^(?P<day_id>\d+)/$', views.detail, name='detail'),
+    # ex: order/42/orders/
+    url(r'^(?P<day_id>\d+)/orders/$', views.orders, name='orders'),
+    # ex: /order/42/order/
+    url(r'^(?P<order_id>\d+/order/$', views.order, name='order'),
 )
--- mensa/order/views.py
+++ mensa/order/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Order index.')
+
+def detail(request, day_id):
+    return HttpResponse("You're looking at Day %s." % day_id)
+
+def orders(request, day_id):
+    return HttpResponse("You're looking at the order summary of Day %s." % day_id)
+
+def order(request, day_id):
+    return HttpResponse("You're ordering for day %s" % day_id)
