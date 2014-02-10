4f855b50dc115f7e9f8235a04d1cd0ad3697c5b3
Thomas Schneider <thosch97@gmail.com>
2014-02-10 13:45:06 +0100

    Made the orders view work

--- mensa/order/views.py
+++ mensa/order/views.py
@@ -5,7 +5,7 @@ from django.template import RequestContext, loader
 from order.models import Day
 
 def index(request):
-    this_week_list = Day.objects.order_by('-date')[:5]
+    this_week_list = Day.objects.all().order_by('-date')[:5]
     template = loader.get_template('orders/index.html')
     context = RequestContext(request, {
         'this_week_list': this_week_list,
